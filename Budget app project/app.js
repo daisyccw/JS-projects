e82ef92932a2af96d99c1df1ab92915aa7d31dd4
Daisy Westphal <daisyccw14@gmail.com>
2019-03-20 15:36:58 +0100

    added current date

--- Budget app project/app.js
+++ Budget app project/app.js
@@ -159,7 +159,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
 
     };
 
@@ -273,6 +274,19 @@ var UIController = (function() {
 
         },
 
+        displayMonth: function() {
+            var now, months, month, year;
+
+            now = new Date();
+
+            months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -370,6 +384,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
