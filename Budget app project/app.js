518e1cfc199e6cca6ce35950009f92a7527b951c
Daisy Westphal <daisyccw14@gmail.com>
2019-03-20 16:04:05 +0100

    changed UI input fields and button colors

--- Budget app project/app.js
+++ Budget app project/app.js
@@ -181,6 +181,12 @@ var UIController = (function() {
         return (type === 'exp' ? '-' :  '+') + ' ' + int + '.' + dec;
     };
 
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
     return {
         getInput: function() {
             return {
@@ -189,8 +195,6 @@ var UIController = (function() {
                 value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
-
-        
 
         addListItem: function(obj, type) {
             var html, newHtml, element;
@@ -255,24 +259,18 @@ var UIController = (function() {
         displayPercentages: function(percentages) {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
-            
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
 
-            nodeListForEach(fields, function(current, index) {
+        nodeListForEach(fields, function(current, index) {
 
-                if (percentages[index] > 0) {
-                current.textContent = percentages[index] + '%';
-                }
-                else {
-                    current.textContent = '---';
-                }
-            });
+            if (percentages[index] > 0) {
+            current.textContent = percentages[index] + '%';
+            }
+            else {
+                current.textContent = '---';
+            }
+        });
 
-        },
+    },
 
         displayMonth: function() {
             var now, months, month, year;
@@ -284,6 +282,21 @@ var UIController = (function() {
 
             year = now.getFullYear();
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+
+        },
+
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue);
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
 
         },
 
@@ -310,6 +323,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
 
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
 
     var updateBudget = function() {
